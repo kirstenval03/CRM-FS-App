cb412f238d7242d2c4bc005fcc4d94d4fc788806
kirstenval03 <kirs0307@gmail.com>
2023-12-10 17:48:36 -0600

    App.js imports for theme + ColorContext

--- src/theme.js
+++ src/theme.js
@@ -161,5 +161,53 @@ export const themeSettings = (mode) => {
                 },  
             }),
         },
+        typography:{
+            fontFamily: ["Montserrat", "sans-serif"].join(","),
+            fontSize: 12,
+            h1: {
+                fontFamily: ["Montserrat", "sans-serif"].join(","),
+                fontSize: 40,
+              },
+              h2: {
+                fontFamily: ["Montserrat", "sans-serif"].join(","),
+                fontSize: 32,
+              },
+              h3: {
+                fontFamily: ["Montserrat", "sans-serif"].join(","),
+                fontSize: 24,
+              },
+              h4: {
+                fontFamily: ["Montserrat", "sans-serif"].join(","),
+                fontSize: 20,
+              },
+              h5: {
+                fontFamily: ["Montserrat", "sans-serif"].join(","),
+                fontSize: 16,
+              },
+              h6: {
+                fontFamily: ["Montserrat", "sans-serif"].join(","),
+                fontSize: 14,
+              },
+        },
     };
 };
+
+// CONTEXT FOR COLOR MODE 
+export const ColorModeContext = createContext({
+    toggleColorMode: () => {},
+  });
+  
+  export const useMode = () => {
+    const [mode, setMode] = useState("dark");
+  
+    const colorMode = useMemo(
+      () => ({
+        toggleColorMode: () =>
+          setMode((prev) => (prev === "light" ? "dark" : "light")),
+      }),
+      []
+    );
+  
+    const theme = useMemo(() => createTheme(themeSettings(mode)), [mode]);
+    return [theme, colorMode];
+  };
