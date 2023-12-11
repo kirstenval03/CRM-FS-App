53b92872046a411801bb8472edcae99c0c8643ae
kirstenval03 <kirs0307@gmail.com>
2023-12-10 20:37:30 -0600

    Topbar

--- src/pages/global/TopBar.jsx
+++ src/pages/global/TopBar.jsx
@@ -22,9 +22,32 @@ const Topbar = ()  => {
         borderRadius="3px"
         >
             <InputBase sx={{ml:2,flex: 1}} placeholder="Search"/>
+            <IconButton type="button" sx={{p:1}}>
+                <SearchIcon/>
+            </IconButton>
         </Box>
 
-        <IconButton sx={{display:"flex"}}></IconButton>
+
+        {/* ICONS */}
+        <Box display="flex">
+            <IconButton onClick={colorMode.toggleColorMode}>
+                {theme.palette.mode === 'dark' ? (
+                    <DarkModeOutlinedIcon/>
+                ) : (
+                    <LightModeOutlinedIcon/>
+                )}
+                
+            </IconButton>
+            <IconButton>
+                <NotificationsOutlinedIcon/>
+            </IconButton>
+            <IconButton>
+                <SettingsOutlinedIcon/>
+            </IconButton>
+            <IconButton>
+                <PersonOutlinedIcon/>
+            </IconButton>
+             </Box>
     </Box>
 }
 export default Topbar
